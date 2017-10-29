c4b96680a833d4823dbe2f38c797491be63a79bd
tanghaichuan <764505165@qq.com>
2017-10-29 22:31:12 +0800

    事件监听

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './index.css'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="footer">Footer</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './index.css'
+
+console.log('0');
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="header">Header</h1>
+            </div>
+        )
+    }
+}
